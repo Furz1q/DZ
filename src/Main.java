9e400784075e2a5e8047497a8e267f796164d9a7
Furz1q <serbek200@mail.ru>
2024-09-22 22:29:08 +0300

    Начало

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,68 @@
-public class Main {
-    public static void main(String[] args) {
+import java.time.LocalDate;
 
-        System.out.println("Задание 1");
+public class Main {
+    public static void checkAndPrint(int year) {
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+    private static boolean isOldDevise ( int clientDeviceYear){
+        int currentYear = 2015;
+        return clientDeviceYear <= currentYear;
+    }
+    private static String giveRecommendVersion(int clientDeviceYear, int clientOS) {
+        String massage = "Установите ";
+        if (isOldDevise(clientDeviceYear)) {
+            massage += "облегченную ";
+        }
+        massage += "версию приложения для ";
+        if (clientOS == 1) {
+            massage += "Android по ссылке";
+        } else {
+            massage += "iOS по ссылке";
+        }
+        return massage;
+    }
+    private static String calculateDeliveryDistance(int deliveryDistance) {
+        String days = "Потребуется дне: ";
+        if (deliveryDistance <= 20){
+            days += "1";
+        }else if (deliveryDistance > 20 && deliveryDistance <= 60){
+            days += "2";
+        }else if (deliveryDistance > 60 && deliveryDistance <= 100){
+            days += "3";
+        }else {
+            days = "Доставки нет";
+        }
+        return days;
+    }
+
+    public static void main(String[] args) {
+        {
+            System.out.println("Задание 1");
+
+            int year = 2023;
+            checkAndPrint(year);
+        }
+
+        {
+            System.out.println("Задание 2");
+
+            int clientOS = 1;
+            int clientDeviceYear = 2015;
+            System.out.println(giveRecommendVersion(2016, 1));
+
+        }
+
+        {
+            System.out.println("Задание 3");
+
+            System.out.println(calculateDeliveryDistance(188));
+
+
+        }
     }
 }
+
